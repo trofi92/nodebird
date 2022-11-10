550ea9b8e037c42c8a866d2255e909bbae965034
lunaslash9249@gmail.com <lunaslash9249@gmail.com>
2022-11-10 14:48:01 +0900

    first commit

---
+++ app/src/routes/page.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+
+router.use((req, res, next) => {
+  res.locals.user = null;
+  res.locals.followerCount = 0;
+  res.locals.followingCount = 0;
+  res.locals.followerIdList = [];
+  next();
+});
+
+router.get("/profile", (req, res) => {
+  res.render("profile", { title: "내 정보 = Nodebird" });
+});
+
+router.get("/join", (req, res) => {
+  res.render("join", { title: "회원가입 - Nodebird" });
+});
+
+router.get("/", (req, res, next) => {
+  const twits = [];
+  res.render("main", { title: "Nodebird", twits });
+});
+
+module.exports = router;
